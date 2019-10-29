f85dc9a1fc27ed026b2305ef0e903499ad8b72ae
Joshua Dyck <joshuadyck77@gmail.com>
2019-10-29 15:16:53 -0700

    Initial commit

---
+++ src/qt/openuridialog.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2011-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <qt/openuridialog.h>
+#include <qt/forms/ui_openuridialog.h>
+
+#include <qt/guiutil.h>
+#include <qt/walletmodel.h>
+
+#include <QUrl>
+
+OpenURIDialog::OpenURIDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OpenURIDialog)
+{
+    ui->setupUi(this);
+    ui->uriEdit->setPlaceholderText("bitcoin:");
+}
+
+OpenURIDialog::~OpenURIDialog()
+{
+    delete ui;
+}
+
+QString OpenURIDialog::getURI()
+{
+    return ui->uriEdit->text();
+}
+
+void OpenURIDialog::accept()
+{
+    SendCoinsRecipient rcp;
+    if(GUIUtil::parseBitcoinURI(getURI(), &rcp))
+    {
+        /* Only accept value URIs */
+        QDialog::accept();
+    } else {
+        ui->uriEdit->setValid(false);
+    }
+}
